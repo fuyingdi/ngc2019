fbe0ad3c50c4bc663bcd4295194092e63370225c
fuyingdi <fuyingdi@126.com>
2019-07-24 14:01:17 +0800

    add:json sample

---
+++ Assets/Scripts/EventCreator.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using LitJson;
+
+public class EventCreator : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
