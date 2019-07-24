6b73c6af9fb8ea0290da163af78a1ea6f093bf75
fuyingdi <fuyingdi@126.com>
2019-07-24 14:09:53 +0800

    add: GameEvent

--- Assets/Scripts/EventCreator.cs
+++ Assets/Scripts/EventCreator.cs
@@ -2,12 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 using LitJson;
+using System.IO;
 
 public class EventCreator : MonoBehaviour
 {
+    public string jsonString;
     void Start()
     {
-        
+        jsonString = File.ReadAllText(Application.dataPath + "/Asset/Event.json");
     }
 
     void Update()
--- Assets/Scripts/GameEvent.cs
+++ Assets/Scripts/GameEvent.cs
@@ -4,5 +4,19 @@ using UnityEngine;
 
 public class GameEvent
 {
+    public GameEvent()
+    {
+
+    }
+
+    public string ShowText { get; set; }
+    public string ResultTextA { get; set; }
+    public string ResultTextB { get; set; }
+
+    public int EventID { get; set; }
+    public int ImageIndex { get; set; }
+
+    public int[] Changes_A = new int[4];
+    public int[] Changes_B = new int[4];
 
 }
