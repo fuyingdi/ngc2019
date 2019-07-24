11017ee35d830191450f812104675504939d87a2
fuyingdi <fuyingdi@126.com>
2019-07-24 15:27:52 +0800

    add: parse json and generate event

--- Assets/Scripts/EventCreator.cs
+++ Assets/Scripts/EventCreator.cs
@@ -6,10 +6,22 @@ using System.IO;
 
 public class EventCreator : MonoBehaviour
 {
-    public string jsonString;
+    public string JsonString;
+    public List<GameEvent> GameEvents= new List<GameEvent>(); //游戏事件池
     void Start()
     {
-        jsonString = File.ReadAllText(Application.dataPath + "/Asset/Event.json");
+        JsonString = File.ReadAllText(Application.dataPath + "\\Events.json");
+        JsonData EventData = JsonMapper.ToObject(JsonString);
+        GameEvents = JsonMapper.ToObject<List<GameEvent>>(JsonString);
+
+        #region
+        //foreach(System.Reflection.PropertyInfo p in GameEvents[0].GetType().GetProperties())
+        //{
+        //    print(p.Name + ":" + p.GetValue(GameEvents[0], null));
+        //}
+        //print(GameEvents[0].EventID);
+        //print(GameEvents[0].Changes_A.people);
+        #endregion
     }
 
     void Update()
--- Assets/Scripts/GameEvent.cs
+++ Assets/Scripts/GameEvent.cs
@@ -16,7 +16,21 @@ public class GameEvent
     public int EventID { get; set; }
     public int ImageIndex { get; set; }
 
-    public int[] Changes_A = new int[4];
-    public int[] Changes_B = new int[4];
+    //public int[] Changes_A = new int[4];
+    //public int[] Changes_B = new int[4];
+    public Changes Changes_A;
+    public Changes Changes_B;
+
+
+    public class Changes
+    {
+        public int people;
+        public int policy;
+        public int military;
+        public int economic;
+    }
+
+
+
 
 }
