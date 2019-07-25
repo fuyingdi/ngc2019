6c52b01cf5993dd4fe127866737115245ff15bde
fuyingdi <fuyingdi@126.com>
2019-07-25 16:10:17 +0800

    add canvas prefab

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,12 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+
     void Start()
     {
-        
+
     }
 
     void Update()
